bd54c59c08a2833b04cca4dd74dfebfbf4e1ab45
Jose Alanya <jose.alanya@gmail.com>
2017-08-11 10:41:57 -0500

    FEAT: Iterators, Generators, and Promises - Using Then when promise is resolved with a value

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function doAsync() {
   let p = new Promise(function (resolve, reject) {
     console.log('in promise code');
     setTimeout(function() {
-      console.log('rejecting...');
-      reject('Database Error');
+      console.log('resolving...');
+      resolve('OK');
     }, 2000);
   });
   return p;
