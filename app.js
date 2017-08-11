1df2aef9b7d0e9c171c3725b7d9a5645f090ed14
Jose Alanya <jose.alanya@gmail.com>
2017-08-11 10:22:01 -0500

    FEAT: Iterators, Generators, and Promises - Promises reject()

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function doAsync() {
   let p = new Promise(function (resolve, reject) {
     console.log('in promise code');
     setTimeout(function() {
-      console.log('resolving...');
-      resolve();
+      console.log('rejecting...');
+      reject();
     }, 2000);
   });
   return p;
